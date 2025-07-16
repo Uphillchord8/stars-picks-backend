0fc00466b58b7ba1811a0fa0253253e6d2a3cbd5
Uphillchord8 <Cole.Davidson8@gmail.com>
2025-07-16 15:47:03 -0400

    Create picksController.js

---
+++ src/controllers/picksController.js
@@ -0,0 +1,32 @@
+// src/controllers/picksController.js
+const Pick = require('../models/Pick');
+
+exports.submitPicks = async (req, res, next) => {
+  try {
+    const { gameId, firstGoalPlayerId, gwGoalPlayerId } = req.body;
+    const filter = { userId: req.user.id, gameId };
+    const update = {
+      firstGoalPlayerId,
+      gwGoalPlayerId,
+      isDefault: false,
+      submittedAt: Date.now(),
+    };
+    const opts = { upsert: true, new: true, setDefaultsOnInsert: true };
+    const pick = await Pick.findOneAndUpdate(filter, update, opts);
+    res.status(201).json(pick);
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getPicksByGame = async (req, res, next) => {
+  try {
+    const picks = await Pick.find({ gameId: req.params.gameId })
+      .populate('userId', 'username avatarUrl')
+      .populate('firstGoalPlayerId', 'name headshotUrl')
+      .populate('gwGoalPlayerId', 'name headshotUrl');
+    res.json(picks);
+  } catch (err) {
+    next(err);
+  }
+};
