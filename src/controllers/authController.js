22c2c52a052f60f48e2c85fc212bd69c051284f6
Uphillchord8 <Cole.Davidson8@gmail.com>
2025-07-16 15:45:34 -0400

    Create authController.js

---
+++ src/controllers/authController.js
@@ -0,0 +1,43 @@
+// src/controllers/authController.js
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const signToken = userId => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '7d' });
+};
+
+exports.register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = await User.create({ username, email, passwordHash: hash });
+    const token = signToken(user._id);
+    res.status(201).json({ user: { id: user._id, username, email }, token });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json({ error: 'Invalid credentials' });
+    const valid = await bcrypt.compare(password, user.passwordHash);
+    if (!valid) return res.status(401).json({ error: 'Invalid credentials' });
+    const token = signToken(user._id);
+    res.json({ user: { id: user._id, username: user.username, email }, token });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getMe = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id).select('-passwordHash');
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
