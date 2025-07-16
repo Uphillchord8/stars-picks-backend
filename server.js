17497e0822c111e65cc776cae0dc826019ce359b
Uphillchord8 <Cole.Davidson8@gmail.com>
2025-07-16 17:54:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((err, req, res, next) => {
 
 // connect to Mongo and start server
 const PORT = process.env.PORT; 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
