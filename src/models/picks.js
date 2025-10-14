0a03f7d18f39f4fd511bdfcb605d632bc6aeccf3
Uphillchord8 <Cole.Davidson8@gmail.com>
2025-10-14 15:37:12 -0400

    Update picks.js

--- src/models/picks.js
+++ src/models/picks.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const pickSchema = new mongoose.Schema({
   userId:            { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  gameId:            { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: true },
+  MongoId:            { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: true },
   gamePk:            { type: Number, required: true, index: true },
   firstGoalPlayerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true },
   gwGoalPlayerId:    { type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true },
